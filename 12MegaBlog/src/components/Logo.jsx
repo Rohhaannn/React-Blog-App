8d0fe4b6b91ab0d9aba9e6a8f2174ae854353eb5
Rohan <shingaderohan96@gmail.com>
2024-03-02 13:08:37 +0530

    Deploying Blog App Mega Project

---
+++ 12MegaBlog/src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Logo({width = "100px"}) {
+  return (
+    <div className='text-center'>
+      <img src="https://cdn-icons-png.flaticon.com/512/1809/1809216.png" width={50} height={50} alt="React Post Logo" />
+    </div>
+  )
+}
+
+export default Logo
