5b82ae649c1fcec1e87c11ccb0ab9df4736b5bd9
Rohan <shingaderohan96@gmail.com>
2024-04-08 16:27:32 +0530

    basic Navbar style done

--- BookStore/Frontend/src/App.jsx
+++ BookStore/Frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Navbar from './components/Navbar'
 
 
 function App() {
@@ -6,7 +7,7 @@ function App() {
 
   return (
     <>
-      <h1> Hello World ! </h1>
+      <Navbar/>
     </>
   )
 }
