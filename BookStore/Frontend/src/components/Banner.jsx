20cbe11b2224d96c78734d3bbeb044f7bc651798
Rohan <shingaderohan96@gmail.com>
2024-04-09 17:36:41 +0530

    navbar style changed on scroll

--- BookStore/Frontend/src/components/Banner.jsx
+++ BookStore/Frontend/src/components/Banner.jsx
@@ -4,7 +4,7 @@ import Book_SALT_Water from '../../public/Book_SALT_Water.avif'
 function Banner() {
   return (
     <>
-      <div className='max-w-screen-2xl container mx-auto md:px-20 px-4 flex flex-col md:flex-row'> 
+      <div className='max-w-screen-2xl container mx-auto md:px-20 px-4 flex flex-col md:flex-row my-10'> 
 
         <div className='w-full md:w-1/2 mt-12 md:mt-32 order-2 md:order-1'> 
           <div className='space-y-12'>
@@ -24,7 +24,7 @@ function Banner() {
             <button className="btn mt-6 btn-secondary">Send</button>
         </div>
 
-        <div className='order-1 w-full md:w-1/2 px-10 mt-7 md:mt-14'>
+        <div className='order-1 w-full md:w-1/2 px-10 mt-20 md:mt-14'>
           <img src={Book_SALT_Water} className='w-75 h-115' alt="Banner Image"/>
         </div>
         
--- BookStore/Frontend/src/components/Navbar.jsx
+++ BookStore/Frontend/src/components/Navbar.jsx
@@ -1,6 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Navbar() {
+  const [sticky, setSticky] = useState(false);
+
+  useEffect(() => {
+    const handleScroll =() => {
+      if(window.scrollY > 0) {
+        setSticky(true)
+      } else {
+        setSticky(false)
+      }
+
+    }
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll)
+    }
+  },[])
+
   const navItems = (
     <>
       <li>
@@ -19,8 +37,10 @@ function Navbar() {
   );
   return (
     <>
-      <div className="max-w-screen-2xl container mx-auto md:px-20 px-4">
-        <div className="navbar bg-base-100">
+      <div className={`max-w-screen-2xl container mx-auto md:px-20 px-4 fixed top-0 left-0 right-0 ${
+        sticky ? "sticky-navbar shadow-md bg-base-200 duration-300 transition-all ease-in-out":""
+      }`}>
+        <div className="navbar bg-base-200">
           <div className="navbar-start">
             <div className="dropdown">
               <div
@@ -110,7 +130,7 @@ function Navbar() {
                 Login
               </a>
             </div>
-            
+
           </div>
         </div>
       </div>
