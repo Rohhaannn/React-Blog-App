7d4a9143dd2836ed2f622d0f2f405dd57a353ca6
Rohan <shingaderohan96@gmail.com>
2024-04-12 16:28:55 +0530

    code updated for 768px window

--- BookStore/Frontend/src/components/FreeBooks.jsx
+++ BookStore/Frontend/src/components/FreeBooks.jsx
@@ -20,18 +20,26 @@ function FreeBooks() {
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToShow: 2,
+          slidesToScroll: 2,
           infinite: true,
           dots: true
         }
       },
       {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 1
+        }
+      },
+      {
         breakpoint: 600,
         settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 1
         }
       },
       {
