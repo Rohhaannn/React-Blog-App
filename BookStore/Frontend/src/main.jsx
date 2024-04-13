3af46b20ad9015ddc6d0eb90d719af3befbc1486
Rohan <shingaderohan96@gmail.com>
2024-04-13 18:01:18 +0530

    router links defined

--- BookStore/Frontend/src/main.jsx
+++ BookStore/Frontend/src/main.jsx
@@ -2,9 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './Home/Home.jsx'
+import Course from './components/Course.jsx'
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout/>}>
+      <Route path='/' element={<Home/>}/>
+      <Route path='course' element={<Course/>}/>
+      {/* <Route path='contact' element={<Contact/>}/>
+      <Route path='about' element={<About/>}/> */}
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    {/* <App /> */}
+    <RouterProvider router={router}/>
+  </React.StrictMode>
+  ,
 )
