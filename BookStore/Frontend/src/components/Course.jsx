69e32fca22929a6a4ced18971c6d5897a9cb8ce0
Rohan <shingaderohan96@gmail.com>
2024-04-13 18:54:22 +0530

    components created for routing test

--- BookStore/Frontend/src/components/Course.jsx
+++ BookStore/Frontend/src/components/Course.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 
 function Course() {
   return (
-    <div>
-      Course
-    </div>
+    <>
+      <div className='max-w-screen-2xl container mx-auto md:px-20 px-4'>
+        <div>
+          <h1 className='text-3xl my-20'> We're delighted to have you here ! </h1>
+          <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia accusamus voluptatum eius perferendis. Illo cumque voluptatibus eius ipsum, provident a consequatur fugit in ullam dicta sit ea molestias labore doloribus amet pariatur ipsam soluta? Enim, sapiente. Fugit velit molestias reprehenderit iusto harum, ab repellat obcaecati ex veniam vero quae laborum vitae earum, nam maiores nulla quas officia magnam. Illo voluptas et impedit placeat vel esse incidunt, blanditiis voluptatum animi quae, sapiente exercitationem sunt nulla nemo ipsam eaque dolorem tempora fuga error tempore deserunt ipsa corporis sit expedita. Nam doloribus eveniet, ad voluptate beatae harum, dolore quos magnam ipsam, voluptates deleniti tempora. Ad magni ipsam nesciunt, expedita sint similique at fuga repellendus eum. Magnam neque ex ducimus, ipsam nobis iste enim cum rem itaque. Cumque unde hic soluta accusantium, temporibus dolore. Ea quasi soluta maiores! Dicta esse quidem non nostrum impedit eveniet commodi dolor nam ab vitae aut, tempora praesentium totam veritatis odio quisquam similique eum omnis inventore atque facere nihil tempore unde? Vitae sunt itaque ipsam quidem! Quae beatae animi perspiciatis at consectetur sint consequuntur, eveniet error iste asperiores quo similique maiores molestias porro rem atque eius quod libero tenetur ipsa adipisci. Vitae mollitia optio accusantium voluptatum fugiat recusandae quas voluptas perferendis natus, officiis, porro laborum autem ad impedit voluptate vel veniam numquam aperiam sequi maxime dolore? Aliquam id excepturi, doloremque repellendus cupiditate fuga dolores ipsa eligendi. Ut voluptatem dignissimos pariatur nihil aliquam voluptas eligendi, voluptate officia eos facilis id ducimus ipsum nesciunt aut temporibus architecto eum reiciendis! Inventore, voluptas.</p>
+        </div>
+      </div>
+    </>
   )
 }
 
