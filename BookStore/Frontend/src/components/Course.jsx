1bd108bcc1899c347eeba0c42b1a88ef23faf373
Rohan <shingaderohan96@gmail.com>
2024-04-17 20:40:01 +0530

    displaying all the books in course component

--- BookStore/Frontend/src/components/Course.jsx
+++ BookStore/Frontend/src/components/Course.jsx
@@ -1,20 +1,31 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Cards from './Cards'
+import list from '../../public/list.json'
 
 function Course() {
   return (
     <>
       <div className='max-w-screen-2xl container mx-auto md:px-20 px-4'>
         <div className='mt-28 items-center justify-center text-center'>
-          <h1 className='text-3xl md:text-4xl'> We're delighted to have you <span className='text-pink-500'> here ! </span>  </h1>
+          <h1 className='text-3xl md:text-4xl'> We're delighted to have you <span className='text-pink-500'> here ! </span> </h1>
 
           <p className='mt-12'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto soluta, perferendis vero deserunt consequuntur sunt in nemo earum nobis natus necessitatibus dolorum assumenda ex, mollitia eos? Voluptatum accusamus nisi dolores.</p>
 
           <Link to="/">
-            <button className="btn mt-6 btn-secondary">Back</button>
+            <button className="btn mt-6 btn-secondary hover:bg-pink-600 duration-300"> Back </button>
           </Link>
 
         </div>
+
+        <div>
+          {
+            list.map((item)=>{
+              <Cards key={item.id} item={item}/>
+            })
+          }
+        </div>
+
       </div>
     </>
   )
