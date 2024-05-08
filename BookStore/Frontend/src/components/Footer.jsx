eac8efa9ed73969592934c36201956e6e4e17bcc
Rohan <shingaderohan96@gmail.com>
2024-05-08 19:14:31 +0530

    bg color removed from footer

--- BookStore/Frontend/src/components/Footer.jsx
+++ BookStore/Frontend/src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function Footer() {
   return (
     <div>
-      <footer className="footer footer-center p-10 bg-base-200 text-base-content rounded">
+      <footer className="footer footer-center p-10  text-base-content rounded">
         <nav className="grid grid-flow-col gap-4">
           <Link to ="/about" className="link link-hover">About us</Link >
           <Link to ="/contact" className="link link-hover">Contact</Link >
