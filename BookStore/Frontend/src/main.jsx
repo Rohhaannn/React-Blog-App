33c0e886ab7db2429653fdd99fa620553c632262
Rohan <shingaderohan96@gmail.com>
2024-05-09 17:59:04 +0530

    Navitems reorder

--- BookStore/Frontend/src/main.jsx
+++ BookStore/Frontend/src/main.jsx
@@ -14,9 +14,9 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<Layout/>}>
       <Route path='/' element={<Home/>}/>
+      <Route path='about' element={<About/>}/>
       <Route path='course' element={<Course/>}/>
       <Route path='contact' element={<Contact/>}/>
-      <Route path='about' element={<About/>}/>
     </Route>
   )
 )
