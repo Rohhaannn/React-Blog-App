3b276dc9d64fb525d68c037e117c8086c8e030e3
Rohan <shingaderohan96@gmail.com>
2024-05-18 18:07:39 +0530

    Login modal updated for light & dark mode

--- BookStore/Frontend/src/components/Login.jsx
+++ BookStore/Frontend/src/components/Login.jsx
@@ -7,7 +7,7 @@ function Login() {
   return (
     <div>
       <dialog id="my_modal_3" className={`modal dark:text-white`}>
-        <div className="modal-box">
+        <div className="modal-box dark:bg-white dark:text-black ">
           <form method="dialog">
             
             <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
@@ -22,7 +22,7 @@ function Login() {
             <input
               type="email"
               placeholder="Enter Your Email"
-              className="w-full px-3 py-1 border rounded-md outline-none"  
+              className="w-full px-3 py-1 border rounded-md outline-none dark:bg-white dark:text-black"  
             />
           </div>
           <div className="mt-4 space-y-2">
@@ -31,7 +31,7 @@ function Login() {
             <input
               type="password"
               placeholder="Enter Your Password"
-              className="w-full px-3 py-1 border rounded-md outline-none"  
+              className="w-full px-3 py-1 border rounded-md outline-none dark:bg-white dark:text-black"  
             />
           </div>
 
