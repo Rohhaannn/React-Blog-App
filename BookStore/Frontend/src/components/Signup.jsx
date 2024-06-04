77898f3ded99b42ae9c0d82ab14360c89a77b42d
Rohan <shingaderohan96@gmail.com>
2024-06-04 17:58:38 +0530

    Signup component in making

--- BookStore/Frontend/src/components/Signup.jsx
+++ BookStore/Frontend/src/components/Signup.jsx
@@ -1,10 +1,75 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Signup() {
   return (
-    <div>
-      <h1 className='mt-10'> Sign Up </h1>
-    </div>
+    <>
+      <div className='flex h-screen items-center justify-center'> 
+      <h1> This is SignUp </h1>
+        <dialog id="my_modal_3" className={`modal border-[2px] border-shadow-md dark:text-white display-block`}>
+          <div className="modal-box dark:bg-white dark:text-black ">
+            <form method="dialog">
+              
+              <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+                ✕
+              </button>
+            </form>
+            {/* <h3 className="font-bold text-2xl text-center "> Sign Up!</h3> */}
+
+            <div className="mt-4 space-y-2">
+              <span> Name </span>
+              <br/>
+              <input
+                type="text"
+                placeholder="Enter Your Email"
+                className="w-full px-3 py-1 border rounded-md outline-none dark:bg-white dark:text-black"  
+              />
+            </div>
+
+            <div className="mt-4 space-y-2">
+              <span> Email </span>
+              <br/>
+              <input
+                type="email"
+                placeholder="Enter Your Email"
+                className="w-full px-3 py-1 border rounded-md outline-none dark:bg-white dark:text-black"  
+              />
+            </div>
+
+            <div className="mt-4 space-y-2">
+              <span> Password </span>
+              <br/>
+              <input
+                type="password"
+                placeholder="Enter Your Password"
+                className="w-full px-3 py-1 border rounded-md outline-none dark:bg-white dark:text-black"  
+              />
+            </div>
+
+            <div className="mt-4 space-y-2">
+              <span> Confirm Password </span>
+              <br/>
+              <input
+                type="password"
+                placeholder="Enter Your Password"
+                className="w-full px-3 py-1 border rounded-md outline-none dark:bg-white dark:text-black"  
+              />
+            </div>
+
+            <div className="items-center text-center space-x-5">
+              <button className="btn mt-6 btn-secondary hover:bg-pink-600 hover:text-white duration-300"> Sign Up </button>
+            </div>
+
+            <div className="items-center text-center mt-7">
+              <p>
+                Already registered ? <Link to="/login" className="text-blue-600 underline cursor-pointer"> Login </Link>
+              </p>
+            </div>
+
+          </div>
+        </dialog>
+      </div>
+    </>
   )
 }
 
